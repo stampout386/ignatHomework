0fee03c8f99846e81a87b5d9bae86f588df6b54c
Mikhail Shestak <stampout386@gmail.com>
2022-01-11 16:42:20 +0300

    6HW

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -2,33 +2,35 @@ import React, {useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
+import s from './common/c4-SuperEditableSpan/SuperEditableSpan.module.css'
 
 function HW6() {
     const [value, setValue] = useState<string>('')
 
     const save = () => {
+        setValue(value)
         saveState<string>('editable-span-value', value)
     }
     const restore = () => {
-        // setValue()
+        let newValue = restoreState('editable-span-value', value);
+        setValue(newValue);
     }
 
     return (
         <div>
-            <hr/>
             homeworks 6
 
             {/*should work (должно работать)*/}
-            <div>
+            <div className={s.homeworkSix}>
+
                 <SuperEditableSpan
                     value={value}
                     onChangeText={setValue}
                     spanProps={{children: value ? undefined : 'enter text...'}}
                 />
+                <SuperButton onClick={save}>save</SuperButton>
+                <SuperButton onClick={restore}>restore</SuperButton>
             </div>
-            <SuperButton onClick={save}>save</SuperButton>
-            <SuperButton onClick={restore}>restore</SuperButton>
-
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperEditableSpan/>*/}
