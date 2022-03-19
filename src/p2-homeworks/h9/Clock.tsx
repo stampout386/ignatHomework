7f6f2308fff7ea426d1ae00124891268880a473c
Mikhail Shestak <stampout386@gmail.com>
2022-03-19 10:21:35 +0300

    HW9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,29 +3,38 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date)
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            const newDate: Date = new Date;
+            setDate(newDate)
+            return newDate
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
+    const hours = date.getHours();
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear()
+
+    const stringTime = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}` // fix with date
+    const stringDate = `${day < 10 ? `0${day}` : day}.${month < 10 ? `0${month}` : month}.${year}` // fix with date
 
     return (
         <div>
